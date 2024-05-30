62deb72c5cd0b8d29565780eea2e26d6dabfcf99
bikram@dey2003 <62688713+modhack2003@users.noreply.github.com>
2024-05-30 17:41:35 +0000

    Love you

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const moment = require('moment');
-const simpleGit = require('simple-git')();
-const random = require('random');
-const jsonfile = require('jsonfile'); // Added missing import
-const FILE_PATH = './data.json';
+import moment from 'moment';
+import simpleGit from 'simple-git';
+import random from 'random';
+import jsonfile from 'jsonfile';
 
-const makeCommit = n => {
-    if(n == 0) return simpleGit.push();
+const FILE_PATH = './data.json';
+const git = simpleGit();
+
+const makeCommit = async (n) => {
+    if (n === 0 ) console.log('committing');
+    if (n === 0) return git.push();
     const x = random.int(0, 54);
     const y = random.int(0, 6);
-    const DATE = moment().subtract(1, 'y').add(1, 'd')
+    const DATE = moment().subtract(2, 'y').add(1, 'd')
                          .add(x, 'w').add(y, 'd').format();
     const data = {
         date: DATE
     };
-
-    console.log(DATE);
-    jsonfile.writeFile(FILE_PATH, data, () => {
-        simpleGit.add([FILE_PATH]).commit(DATE, {'--date': DATE }).then(() => {
-            return makeCommit(n - 1); // Added return to recursive call
-        });
-    });
+   
+    console.log(n);
+    await jsonfile.writeFile(FILE_PATH, data);
+    await git.add([FILE_PATH]);
+    await git.commit(DATE, {'--date': DATE });
+    makeCommit(n - 1); 
 }
+
 
 makeCommit(500);
