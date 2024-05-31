4677bf3e7300ef2bfede0b21a6a41beb685e2c5c
modhack2003 <modhack2003@users.noreply.github.com>
2024-05-31 13:27:53 +0530

    done

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const moment = require('moment');
 const simpleGit = require('simple-git')();
 const jsonfile = require('jsonfile');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const PORT = process.env.PORT || 3000;
 const FILE_PATH = "./data.json";
 
@@ -13,6 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -21,48 +27,55 @@ function getRandomInt(min, max) {
 }
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home');
 });
 
-app.post('/start-commit', async (req, res) => {
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+app.post('/start-commit', (req, res) => {
     const { commitCount, commitYear, username, token } = req.body;
 
     simpleGit.addConfig('user.name', username);
     simpleGit.addConfig('user.email', `${username}@users.noreply.github.com`);
     simpleGit.addConfig('hub.token', token);
 
-    const makeCommitsAndPush = async (n) => {
-        for (let i = 0; i < n; i++) {
-            console.log(`Committing ${i + 1} of ${commitCount}`);
-            const x = getRandomInt(0, 54);
-            const y = getRandomInt(0, 6);
-            const DATE = moment().year(commitYear).startOf('year').add(x, 'w').add(y, 'd').format();
-            console.log(`Commit date: ${DATE}`);
-            const data = { date: DATE };
+    const makeCommit = n => {
+        if (n == 0) {
+            io.emit('status', 'All commits completed');
+            return;
+        }
 
-            await new Promise((resolve, reject) => {
-                jsonfile.writeFile(FILE_PATH, data, (err) => {
-                    if (err) reject(err);
-                    else resolve();
-                });
+        const x = getRandomInt(0, 54);
+        const y = getRandomInt(0, 6);
+        const DATE = moment().year(commitYear).startOf('year').add(x, 'w').add(y, 'd').format();
+        const data = { date: DATE };
+
+        jsonfile.writeFile(FILE_PATH, data, () => {
+            simpleGit.add([FILE_PATH]).commit(DATE, { '--date': DATE }, (err, result) => {
+                if (err) {
+                    console.error('Commit failed:', err);
+                    io.emit('status', 'Commit failed: ' + err);
+                } else {
+                    console.log('Commit successful:', result);
+                    const progress = ((commitCount - n) / commitCount) * 100;
+                    io.emit('status', `Commit ${commitCount - n}/${commitCount} completed (${progress.toFixed(2)}%)`);
+                    makeCommit(n - 1);
+                }
             });
-
-            await simpleGit.add([FILE_PATH]);
-            await simpleGit.commit(DATE, { '--date': DATE });
-        }
-
-        try {
-            await simpleGit.push();
-            console.log('Push successful!');
-        } catch (error) {
-            console.error('Push failed:', error);
-        }
+        });
     };
 
-    makeCommitsAndPush(commitCount);
-    res.send('Commits and push started!');
+    makeCommit(commitCount);
+
+    res.render('home', {commitCount});
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
